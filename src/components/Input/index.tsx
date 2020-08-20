63d7fde10cd2e219f04b5c0e1057996ae12e5353
Hilderlan <hilakt9698@gmail.com>
2020-08-20 05:22:33 -0300

    Tests on the Input component and on the Signin page

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -51,6 +51,7 @@ const Input: React.FC<InputProps> = ({
       isFilled={isFilled}
       isFocused={isFocused}
       style={containerStyle}
+      data-testid="input-container"
     >
       {Icon && <Icon size={20} />}
       <input
